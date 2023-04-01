df1761b48d38d0fa21dc90596a9b8efb09fce063
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-04-01 12:02:15 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,72 @@
-
+// Brawl Stars Cheat APK dengan menggunakan Nodejs sehingga mempermudah dalam proses deploy
+//-------[ FILE CONST ]--------
+require("./brawlstars.json")
+require("./config.js")
+require("./index.js")
+const {
+  default: brawlstarsApk,
+  cheatGames,
+  modApk,
+  getHacked,
+  makememoryHacked,
+} required("./memory.js")
+//--------[ FILE REQUEST ]--------
+const {
+  var requesmakeUrl("https://supercell.com/en/games/brawlstars")
+  var makecheatUrl("https://supercell.com/games/brawlstars.js")
+  var request("./index.js")
+  var officialUrl("https://supercell.com/games/brawlstars.min.js.pkg")
+}
+//--------[ FILE CASE ]--------
+{
+  case 'hacked':{
+    if requiredselfHosted, makehackedGames, hackedGems, hackedMoney, hackedstarPoin: {true.selfHosted("hackCoin"{100000}),
+      true.selfHosted("hackGems"{500000}),
+      true.selfHosted("hackstarPoin"{650000}),
+      true.selfHosted("hackTrofi"{59900})}
+  {false.publicHosted("coinMakes"{100000}),
+   false.publicHosted("gemsMakes"{500000}),
+   false.publicHosted("starpoinMakes"{650000}),
+   false.publicHosted("trofiMakes"{59900})}
+ break
+  }
+}
+{
+  case 'hackCoin':{
+    if requiredhackCoin(lang.hackCoin),
+    if requiredhackCoin(true.selfHosted),
+    if requiredhackCoin("config.js"),
+    if ("true") // ubah ke true untuk hack koin brawl stars kamu
+  break
+  }
+}
+{
+  case 'hackGems':{
+    if requiredhackGems(lang.hackGems),
+    if requiredhackGems(true.selfHosted),
+    if requiredhackGems("config.js"),
+    if ("true") // ubah ke true untuk hack gems brawl stars kamu
+  break
+  }
+}
+{
+  case 'hackTrofi':{
+    if requiredhackTrofi(lang.hackTrofi),
+    if requiredhackTrofi(true.selfHosted),
+    if requiredhackTrofi("config.js"),
+    if ("true") // uabh ke true untuk hack trofi punya kamu
+  break
+  }
+}
+//--------[ FILE ENCODE ]--------
+<!DOCTYPE Html>
+<html lang="en">
+<head>
+  <title>BRAWL STARS CHEATERS</title>
+  <h3>Website cheat untuk <strong>Brawl Stars</strong> Gunakan website ini sekali saja. Jika digunakan berulang kali mungkin akan membuat fatal.</h3>
+  <label>Ussername</label>
+  <input type="text" name="nama" placeholder="Ussername Anda" required/>
+  <br>
+  <label>Kode Profil</label>
+  <input type="number" name="kode" placeholder="Kode Profile Anda" required/>
+</head>
