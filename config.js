5f16880d334fe302f88d1f20f71879a90ae467db
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-04-01 12:15:00 +0700

    update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+global.Trofi = "85000" // Total cheat trofi yang kamu mau, maksimal 85K trofi
+global.Money = "450000" // Total cheat uang yang kamu mau
+global.Gems = "850000" // Total cheat gems yang kamu mau
+global.starPoint = "350000" // Total cheat starpoint yang kamy mau
 
+redirectUrl={"./server.js"}
